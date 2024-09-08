eafaa71722b1f45380985dd570e7781d8152875a
yusuf sertel <59843626+ysfsrtl123@users.noreply.github.com>
2024-09-08 23:32:52 +0300

    Update database.js

--- public/utility/database.js
+++ public/utility/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
    host:'localhost',
    user: 'root',
-   database: 'nodeapp',
-   password: 'memet1976'
+   database: '*****',
+   password: '****'
 
 });
 
